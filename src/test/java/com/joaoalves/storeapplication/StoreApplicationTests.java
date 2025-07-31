296bcbc2455440116f8b5b7aa3a9660f1874fdff
wttjoao <joaoalves1102@outlook.pt>
2025-07-31 07:18:20 +0100

    first commit

---
+++ src/test/java/com/joaoalves/storeapplication/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaoalves.storeapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
